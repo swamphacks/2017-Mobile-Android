6e8beb8fd9100ab36686d14c98897c56e0350c44
Nikhil Thota <nt92@ufl.edu>
2017-01-07 10:38:25 -0500

    bad styling on filter. will fix later

--- app/src/main/java/com/swamphacks/swamphacks_android/announcements/FilterDialogFragment.java
+++ app/src/main/java/com/swamphacks/swamphacks_android/announcements/FilterDialogFragment.java
@@ -1,11 +1,16 @@
 package com.swamphacks.swamphacks_android.announcements;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.LauncherActivity;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.swamphacks.swamphacks_android.MainActivity;
 import com.swamphacks.swamphacks_android.R;
@@ -22,6 +27,7 @@ public class FilterDialogFragment extends DialogFragment{
         this.listener = listener;
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Log.d("filterlist", "" + selected[0] + " " + selected[1]);
@@ -55,6 +61,17 @@ public class FilterDialogFragment extends DialogFragment{
                     }
                 });
 
-        return builder.create();
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        Button okButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        Button cancelButton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        ListView listView = alertDialog.getListView();
+
+        okButton.setBackground(getResources().getDrawable(R.drawable.button_blue_white_border));
+        cancelButton.setBackground(getResources().getDrawable(R.drawable.button_blue_white_border));
+        listView.setBackgroundColor(R.color.gradient_start);
+
+        return alertDialog;
     }
 }
